0b7456a1a874cb995aeb2b91a590ee5738157dc3
Sijan1992 <seasonneupane@gmail.com>
2021-06-14 11:56:55 +0200

    Updated Weights

--- model.py
+++ model.py
@@ -114,8 +114,8 @@ def train(dataset_name):
     cfg.DATALOADER.NUM_WORKERS = 2
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
     cfg.SOLVER.IMS_PER_BATCH = 2
-    cfg.SOLVER.BASE_LR = 0.01  # pick a good LR # learing rate was 0.01,0.02, 0.00025
-    cfg.SOLVER.MAX_ITER =300    # 500,1000,300 was before, and 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+    cfg.SOLVER.BASE_LR = 0.0125  # pick a good LR # learing rate was 0.01,0.02, 0.00025
+    cfg.SOLVER.MAX_ITER =1500   # 500,1000,300 was before, and 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
     # cfg.SOLVER.STEPS = []        # do not decay learning rate
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (paprika). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
